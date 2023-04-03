05b3cb2b7612d80130b44e795053b0c327f65404
Paris Liakos <paris.liakos@gmail.com>
2023-04-03 22:46:46 +0300

    04 - Rustacean and crate models

---
+++ src/schema.rs
@@ -0,0 +1,29 @@
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    crates (id) {
+        id -> Int4,
+        rustacean_id -> Int4,
+        code -> Varchar,
+        name -> Varchar,
+        version -> Varchar,
+        description -> Nullable<Text>,
+        created_at -> Timestamp,
+    }
+}
+
+diesel::table! {
+    rustaceans (id) {
+        id -> Int4,
+        name -> Varchar,
+        email -> Varchar,
+        created_at -> Timestamp,
+    }
+}
+
+diesel::joinable!(crates -> rustaceans (rustacean_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    crates,
+    rustaceans,
+);
