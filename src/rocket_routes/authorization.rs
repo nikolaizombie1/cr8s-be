0d1e604dc22dae9b1a2dfa3ed128e54975a317bc
Paris Liakos <paris.liakos@gmail.com>
2023-04-09 23:11:02 +0300

    19 - storing session id and login endpoint test

--- src/rocket_routes/authorization.rs
+++ src/rocket_routes/authorization.rs
@@ -1,20 +1,27 @@
-use rocket::{serde::json::{Json, serde_json::json, Value}, response::status::Custom};
-use rocket_db_pools::Connection;
+use rocket::{serde::json::{Json, serde_json::json, Value}, response::status::Custom, http::Status};
+use rocket_db_pools::{Connection, deadpool_redis::redis::AsyncCommands};
 
 use crate::{repositories::UserRepository, auth};
 
 use super::{DbConn, server_error, CacheConn};
 
 #[rocket::post("/login", format="json", data="<credentials>")]
-pub async fn login(credentials: Json<auth::Credentials>, db: DbConn, cache: Connection<CacheConn>) -> Result<Value, Custom<Value>> {
-    db.run(move |c| {
-        UserRepository::find_by_username(c, &credentials.username)
-            .map(|user| {
-                if let Ok(token) = auth::authorize_user(&user, &credentials) {
-                    return json!(token)
-                }
-                json!("Unauthorized")
-            })
+pub async fn login(credentials: Json<auth::Credentials>, db: DbConn, mut cache: Connection<CacheConn>) -> Result<Value, Custom<Value>> {
+    let username = credentials.username.clone();
+    let user = db.run(move |c| {
+        UserRepository::find_by_username(c, &username)
             .map_err(|e| server_error(e.into()))
-    }).await
+    }).await?;
+
+    let session_id = auth::authorize_user(&user, &credentials)
+        .map_err(|_| Custom(Status::Unauthorized, json!("Wrong credentials")))?;
+
+    cache.set_ex::<_, _, ()>(
+        format!("sessions/{}", session_id), 
+        user.id, 
+        3*60*60
+    )
+    .await
+    .map(|_| json!({"token": session_id}))
+    .map_err(|e| server_error(e.into()))
 }
